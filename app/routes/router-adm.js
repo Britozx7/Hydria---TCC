b4d07df6223751ee1b0737e40e9e2b5788713ae5
Guilherme de Brito Santos <guilhermebrito2110@gmail.com>
2025-10-19 13:24:24 -0300

    Area do Adminstrador feito a Area do admnistrador

--- app/routes/router-adm.js
+++ app/routes/router-adm.js
@@ -1,6 +1,26 @@
-var express = require('express');
-var router = express.Router(); 
+const express = require('express');
+const router = express.Router(); 
 
 router.get('/', function(req, res) {
-    res.render('pages/index')
-})
+    res.render('pages/index-adm')
+});
+
+router.get('/usuarios-adm', function(req, res) {
+    res.render('pages/usuarios-adm')
+});
+
+router.get('/newsletter-adm', function(req, res) {
+    res.render('pages/newsletter-adm')
+});
+
+router.get('/ongs-adm', function(req, res) {
+    res.render('pages/ongs-adm')
+});
+
+router.get('/suporte-adm', function(req, res) {
+    res.render('pages/suporte-adm')
+});
+
+
+
+module.exports = router;
